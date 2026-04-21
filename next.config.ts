cad38973beb47b4082021cce5b3a75a023e7b866
Rohit Rathod <rohitrathod6132@gmail.com>
2026-04-21 11:39:36 +0530

    fix: set output configuration to standalone in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   poweredByHeader: false,
   compress: true,
 
